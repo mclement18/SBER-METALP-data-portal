1e98892c9b170c552301bb5aa17a0dd5f009a49f
clement <clement@tsf-436-wpa-0-239.epfl.ch>
2020-07-15 12:01:30 +0200

    Create NavBar UI with some firt styling

--- App.R
+++ App.R
@@ -1,7 +1,27 @@
 library(shiny)
+library(sass)
 
-ui <- basicPage()
+sass(
+  sass_file('assets/sass/main.scss'), 
+  output = 'www/main.css',
+  options = sass_options(output_style = 'compressed')
+)
 
-server <- function(input, output) {}
+
+ui <- tagList(
+  tags$head(
+    tags$link(href = 'main.css', rel = 'stylesheet', type = 'text/css')
+  ),
+  navbarPage(
+    htmlTemplate('html_components/logo.html'),
+    windowTitle = 'METALP DATA PORTAL',
+    tabPanel('Home'),
+    tabPanel('Visualisation'),
+    tabPanel('Data Input'),
+    tabPanel('Tools')
+  )
+)
+
+server <- function(input, output, session) {}
 
 shinyApp(ui, server)
