7f90e27c6c75bd355a4f6977b4001278940f3664
clement <clement@tsf-436-wpa-0-239.epfl.ch>
2020-07-15 15:37:18 +0200

    Create custom navbarPage function to implement content-wrapper and footer

--- App.R
+++ App.R
@@ -7,19 +7,32 @@ sass(
   options = sass_options(output_style = 'compressed')
 )
 
+navbarPageWithWrapper <- function(navbarPageOutput, footer = NULL) {
+  navbarPageOutput[[3]][[1]]$children[[1]]$attribs$class <- str_interp('${navbarPageOutput[[3]][[1]]$children[[1]]$attribs$class} content-wrapper')
+  navbarPageOutput[[3]][[2]]$attribs$class <- str_interp('${navbarPageOutput[[3]][[2]]$attribs$class} content-wrapper')
+  
+  if(!is.null(footer)) {
+    navbarPageOutput[[3]][[length(navbarPageOutput[[3]]) + 1]] <- footer
+  }
+  
+  return(navbarPageOutput)
+}
+
 ui <- tagList(
   tags$head(
     tags$link(href = 'main.css', rel = 'stylesheet', type = 'text/css')
   ),
   tags$body(class = 'footer-to-bottom-container'),
-  navbarPage(
-    htmlTemplate('html_components/logo.html'),
-    windowTitle = 'METALP DATA PORTAL',
-    tabPanel('Home'),
-    tabPanel('Visualisation'),
-    tabPanel('Data Input'),
-    tabPanel('Tools'),
-    footer = htmlTemplate('html_components/footer.html')
+  navbarPageWithWrapper(
+    navbarPage(
+      htmlTemplate('html_components/logo.html'),
+      windowTitle = 'METALP DATA PORTAL',
+      tabPanel('Home'),
+      tabPanel('Visualisation'),
+      tabPanel('Data Input'),
+      tabPanel('Tools')
+    ),
+    htmlTemplate('html_components/footer.html')
   )
 )
 
