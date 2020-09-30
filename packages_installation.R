46035193280c193b6541cc97f60bd5917e86bc97
Mathieu Clément <mcfly1893@gmail.com>
2020-09-30 11:34:14 +0200

    Create packages installation script for deployment

---
+++ packages_installation.R
@@ -0,0 +1,20 @@
+install.packages(
+  pkgs = c("shinyjs", 
+           "shinyWidgets", 
+           "shinybusy", 
+           "shinycssloaders", 
+           "sass", 
+           "jsonlite", 
+           "readr", 
+           "stringr", 
+           "ggplot2", 
+           "Cairo", 
+           "data.table", 
+           "lubridate", 
+           "forcats", 
+           "tidyr", 
+           "magrittr", 
+           "dplyr"
+  ),
+  repos = 'https://cran.rstudio.com/'
+)
