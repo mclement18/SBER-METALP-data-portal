d289c18ccefa22a4bbfd83a5ff38bea6b759687d
Mathieu Clément <mcfly1893@gmail.com>
2020-11-04 14:34:35 +0100

    Implement some custom renderer, validator and hooks callback for the rhandsontable and custom style

---
+++ assets/js/custom_handsontable.js
@@ -0,0 +1,109 @@
+const CustomHandsontable = {};
+
+// Create a time validator
+CustomHandsontable.timeFormatValidator = function (value, callback) {
+    callback(/^\d{2}:\d{2}:\d{2}$/.test(value));
+};
+
+
+// Create a time renderer
+CustomHandsontable.dateRenderer = function (hotInstance, td, row, column, prop, value, cellProperties) {
+    // Optionally include `BaseRenderer` which is responsible for adding/removing CSS classes to/from the table cells.
+    Handsontable.renderers.BaseRenderer.apply(this, arguments);
+
+    // Time renderer
+    // If time format is 'DD/MM/YYYY' convert to 'YYYY-MM-DD'
+    if (/^\d{2}\/\d{2}\/\d{4}$/.test(value)) {
+        value = value.split('/').reverse().join('-');
+    }
+    // Set value
+    td.textContent = value;
+};
+
+// Set table input
+CustomHandsontable.setInputId = function (tableDOM, attrName, inputId) {
+    tableDOM.dataset[attrName] = inputId;
+};
+
+// Create an onChange hook callback
+CustomHandsontable.onChangeCallback = function (changes) {
+    if (changes) {
+        const hot = this;
+        const inputId = hot.rootElement.dataset.onChangeId;
+        const message = [];
+
+        changes.forEach(([row, col, oldValue, newValue]) => {
+            if (oldValue !== newValue) {
+                hot.getCellValidator.bind(hot)(row, col)(newValue, (valid) => {
+                    if (valid) {
+                        message.push({
+                            row: row + 1,
+                            column: col + 1,
+                            value: newValue
+                        });
+        
+                        hot.setCellMeta.bind(hot)(row, col, 'className', 'changed');
+                        hot.render.bind(hot)();
+                    }
+                });
+            }
+        });
+
+        Shiny.setInputValue(inputId, JSON.stringify(message), {priority: "event"});
+    }
+};
+
+// Create an onChange hook
+CustomHandsontable.onChange = function (tableDOM, hot, inputId) {
+    // Set onChangeInputId
+    this.setInputId(tableDOM, 'onChangeId', inputId);
+    // Remove hook in case of multiple rendering from shiny
+    hot.removeHook('afterChange', CustomHandsontable.onChangeCallback);
+    // Add hook
+    hot.addHook('afterChange', CustomHandsontable.onChangeCallback);
+};
+
+// Create afterSelectionEnd hook callback for row deletion purpose
+CustomHandsontable.afterSelectionCallback = function (rowStart, colStart, rowEnd, colEnd, selectionLayerLevel) {
+    const inputId = this.rootElement.dataset.afterSelectionId;
+    const message = {
+        start: null,
+        end: null
+    };
+
+    if (rowStart === rowEnd) {
+        message.start = rowStart;
+    } else if (rowStart < rowEnd) {
+        message.start = rowStart;
+        message.end = rowEnd;
+    } else if (rowStart > rowEnd) {
+        message.start = rowEnd;
+        message.end = rowStart;
+    }
+
+    Shiny.setInputValue(inputId, message, {priority: "event"});
+};
+
+// Create afterDeselect hook callback to remove the selection
+CustomHandsontable.afterDeselectCallback = function () {
+    const inputId = this.rootElement.dataset.afterSelectionId;
+    const message = {
+        start: null,
+        end: null
+    };
+    setTimeout(() => {
+        Shiny.setInputValue(inputId, message, {priority: "event"});
+    }, 1000);
+};
+
+// Create afterSelectionEnd hook for row deletion purpose
+CustomHandsontable.afterSelection = function (tableDOM, hot, inputId) {
+    // Set onChangeInputId
+    this.setInputId(tableDOM, 'afterSelectionId', inputId);
+    // Remove hooks in case of multiple rendering from shiny
+    hot.removeHook('afterSelectionEnd', CustomHandsontable.afterSelectionCallback);
+    hot.removeHook('afterDeselect', CustomHandsontable.afterDeselectCallback);
+    // Add hooks
+    hot.addHook('afterSelectionEnd', CustomHandsontable.afterSelectionCallback);
+    hot.addHook('afterDeselect', CustomHandsontable.afterDeselectCallback);
+};
