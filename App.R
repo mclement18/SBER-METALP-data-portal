ea75db4d11d24cb626a27f5c6303441d9714a2e3
Mathieu Clément <mcfly1893@gmail.com>
2021-01-14 14:29:55 +0100

    Update the development session info

--- App.R
+++ App.R
@@ -13,25 +13,26 @@
 #   [1] stats     graphics  grDevices utils     datasets  methods   base     
 # 
 # other attached packages:
-#   [1] rhandsontable_0.3.7   DT_0.16               dbplyr_1.4.4          pool_0.1.4.3         
-#   [5] DBI_1.1.0             dplyr_1.0.2           magrittr_1.5          tidyr_1.1.2          
-#   [9] forcats_0.5.0         lubridate_1.7.9       data.table_1.13.2     Cairo_1.5-12.2       
-#  [13] ggplot2_3.3.2         stringr_1.4.0         jsonlite_1.7.1        shinycssloaders_1.0.0
-#  [17] shinybusy_0.2.2       shinyWidgets_0.5.4    shinyjs_2.0.0         shiny_1.5.0          
+#   [1] future_1.20.1         promises_1.1.1        rhandsontable_0.3.7   DT_0.16               dbplyr_1.4.4         
+#   [6] pool_0.1.4.3          DBI_1.1.0             dplyr_1.0.2           magrittr_1.5          tidyr_1.1.2          
+#  [11] forcats_0.5.0         lubridate_1.7.9       data.table_1.13.2     Cairo_1.5-12.2        ggplot2_3.3.2        
+#  [16] stringr_1.4.0         jsonlite_1.7.1        shinycssloaders_1.0.0 shinybusy_0.2.2       shinyWidgets_0.5.4   
+#  [21] shinyjs_2.0.0         shiny_1.5.0          
 # 
 # loaded via a namespace (and not attached):
-#   [1] Rcpp_1.0.4.6      prettyunits_1.1.1 ps_1.3.3          assertthat_0.2.1  rprojroot_1.3-2  
-#   [6] digest_0.6.25     packrat_0.5.0     mime_0.9          R6_2.4.1          backports_1.1.8  
-#  [11] pillar_1.4.4      rlang_0.4.8       rstudioapi_0.11   callr_3.4.3       blob_1.2.1       
-#  [16] desc_1.2.0        devtools_2.3.1    RMySQL_0.10.20    readr_1.4.0       htmlwidgets_1.5.1
-#  [21] munsell_0.5.0     compiler_4.0.2    httpuv_1.5.4      pkgconfig_2.0.3   pkgbuild_1.0.8   
-#  [26] htmltools_0.5.0   tidyselect_1.1.0  tibble_3.0.1      fansi_0.4.1       crayon_1.3.4     
-#  [31] withr_2.2.0       later_1.1.0.1     grid_4.0.2        xtable_1.8-4      gtable_0.3.0     
-#  [36] lifecycle_0.2.0   scales_1.1.1      cli_2.0.2         stringi_1.4.6     fs_1.4.2         
-#  [41] promises_1.1.1    remotes_2.2.0     testthat_2.3.2    ellipsis_0.3.1    generics_0.0.2   
-#  [46] vctrs_0.3.4       tools_4.0.2       glue_1.4.1        purrr_0.3.4       hms_0.5.3        
-#  [51] rsconnect_0.8.16  processx_3.4.2    pkgload_1.1.0     fastmap_1.0.1     colorspace_1.4-1 
-#  [56] sessioninfo_1.1.1 memoise_1.1.0     sass_0.2.0        usethis_1.6.1  
+#   [1] nlme_3.1-148      fs_1.4.2          usethis_1.6.1     devtools_2.3.1    rprojroot_1.3-2   tools_4.0.2      
+#   [7] backports_1.1.8   R6_2.4.1          mgcv_1.8-31       colorspace_1.4-1  withr_2.2.0       tidyselect_1.1.0 
+#  [13] sodium_1.1        prettyunits_1.1.1 processx_3.4.2    RMySQL_0.10.20    compiler_4.0.2    cli_2.0.2        
+#  [19] desc_1.2.0        labeling_0.3      sass_0.2.0        scales_1.1.1      DEoptimR_1.0-8    robustbase_0.93-6
+#  [25] readr_1.4.0       callr_3.4.3       digest_0.6.25     pkgconfig_2.0.3   htmltools_0.5.0   parallelly_1.21.0
+#  [31] sessioninfo_1.1.1 bigleaf_0.7.1     fastmap_1.0.1     htmlwidgets_1.5.1 rlang_0.4.8       rstudioapi_0.11  
+#  [37] farver_2.0.3      generics_0.0.2    crosstalk_1.1.0.1 Matrix_1.2-18     Rcpp_1.0.4.6      munsell_0.5.0    
+#  [43] fansi_0.4.1       lifecycle_0.2.0   stringi_1.4.6     yaml_2.2.1        pkgbuild_1.0.8    grid_4.0.2       
+#  [49] blob_1.2.1        parallel_4.0.2    listenv_0.8.0     crayon_1.3.4      lattice_0.20-41   splines_4.0.2    
+#  [55] hms_0.5.3         ps_1.3.3          pillar_1.4.4      codetools_0.2-16  pkgload_1.1.0     glue_1.4.1       
+#  [61] packrat_0.5.0     remotes_2.2.0     vctrs_0.3.4       solartime_0.0.1   httpuv_1.5.4      testthat_2.3.2   
+#  [67] gtable_0.3.0      purrr_0.3.4       assertthat_0.2.1  mime_0.9          xtable_1.8-4      later_1.1.0.1    
+#  [73] rsconnect_0.8.16  tibble_3.0.1      memoise_1.1.0     globals_0.13.1    ellipsis_0.3.1   
 
 
 ## Load Libraries #################################################################
